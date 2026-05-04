79f9a4275196737f187edc901043c6d8e60da47f
Jean-Fischer <jeanfischer0@gmail.com>
2026-05-04 19:41:02 +0200

    fix sonarqube

--- Sharpen.Analyzer/Sharpen.Analyzer/Sharpen.Analyzer/Common/EquivalentAsynchronousMethodSignatureComparer.cs
+++ Sharpen.Analyzer/Sharpen.Analyzer/Sharpen.Analyzer/Common/EquivalentAsynchronousMethodSignatureComparer.cs
@@ -8,57 +8,71 @@ internal static class EquivalentAsynchronousMethodSignatureComparer
 {
     public static bool IsAsynchronousEquivalent(IMethodSymbol? potentialEquivalent, IMethodSymbol method)
     {
-        if (potentialEquivalent == null)
-            return false;
+        return potentialEquivalent is not null &&
+               !potentialEquivalent.ReturnsVoid &&
+               HasCompatibleReturnType(potentialEquivalent, method) &&
+               HasCompatibleParameters(potentialEquivalent, method);
+    }
 
-        if (potentialEquivalent.ReturnsVoid)
-            return false;
-
+    private static bool HasCompatibleReturnType(IMethodSymbol potentialEquivalent, IMethodSymbol method)
+    {
         if (method.ReturnsVoid)
-        {
-            if (!EquivalentAsynchronousMethodMetadata.KnownAwaitableTypes.Any(awaitableType =>
-                    awaitableType.IsVoidEquivalent &&
-                    awaitableType.RepresentsType(potentialEquivalent.ReturnType)))
-            {
-                return false;
-            }
-        }
-        else
-        {
-            if (potentialEquivalent.ReturnType is not INamedTypeSymbol potentialEquivalentReturnType)
-                return false;
+            return HasVoidEquivalentReturnType(potentialEquivalent.ReturnType);
 
-            if (potentialEquivalentReturnType.Arity != 1)
-                return false;
+        var awaitableReturnType = GetAwaitableReturnType(potentialEquivalent.ReturnType);
+        if (awaitableReturnType == null)
+            return false;
 
-            var returnedKnownAwaitableType = EquivalentAsynchronousMethodMetadata.KnownAwaitableTypes
-                .FirstOrDefault(awaitableType =>
-                    awaitableType.RepresentsType(potentialEquivalentReturnType.ConstructedFrom));
-            if (returnedKnownAwaitableType == null)
-                return false;
+        var (potentialEquivalentReturnType, returnedKnownAwaitableType) = awaitableReturnType.Value;
+        return returnedKnownAwaitableType.WrapsReturnType()
+            ? SymbolEqualityComparer.Default.Equals(method.ReturnType, potentialEquivalentReturnType.TypeArguments[0])
+            : MethodTypeArgumentMatches(potentialEquivalentReturnType, method.ReturnType);
+    }
 
-            if (returnedKnownAwaitableType.WrapsReturnType())
-            {
-                if (!SymbolEqualityComparer.Default.Equals(method.ReturnType, potentialEquivalentReturnType.TypeArguments[0]))
-                    return false;
-            }
-            else
-            {
-                if (method.ReturnType is not INamedTypeSymbol { Arity: 1 } methodReturnType)
-                    return false;
+    private static bool HasVoidEquivalentReturnType(ITypeSymbol returnType)
+    {
+        return EquivalentAsynchronousMethodMetadata.KnownAwaitableTypes.Any(awaitableType =>
+            awaitableType.IsVoidEquivalent &&
+            awaitableType.RepresentsType(returnType));
+    }
 
-                if (!SymbolEqualityComparer.Default.Equals(methodReturnType.TypeArguments[0], potentialEquivalentReturnType.TypeArguments[0]))
-                    return false;
-            }
-        }
+    private static (INamedTypeSymbol ReturnType, KnownAwaitableTypeInfo AwaitableType)? GetAwaitableReturnType(
+        ITypeSymbol returnType)
+    {
+        if (returnType is not INamedTypeSymbol { Arity: 1 } namedReturnType)
+            return null;
 
+        var returnedKnownAwaitableType = EquivalentAsynchronousMethodMetadata.KnownAwaitableTypes
+            .FirstOrDefault(awaitableType => awaitableType.RepresentsType(namedReturnType.ConstructedFrom));
+
+        return returnedKnownAwaitableType == null ? null : (namedReturnType, returnedKnownAwaitableType);
+    }
+
+    private static bool MethodTypeArgumentMatches(
+        INamedTypeSymbol potentialEquivalentReturnType,
+        ITypeSymbol methodReturnType)
+    {
+        return methodReturnType is INamedTypeSymbol { Arity: 1 } namedMethodReturnType &&
+               SymbolEqualityComparer.Default.Equals(
+                   namedMethodReturnType.TypeArguments[0],
+                   potentialEquivalentReturnType.TypeArguments[0]);
+    }
+
+    private static bool HasCompatibleParameters(IMethodSymbol potentialEquivalent, IMethodSymbol method)
+    {
         var numberOfParameters = method.Parameters.Length;
-        if (!(potentialEquivalent.Parameters.Length == numberOfParameters ||
-              potentialEquivalent.Parameters.Length == numberOfParameters + 1))
+        if (potentialEquivalent.Parameters.Length != numberOfParameters &&
+            potentialEquivalent.Parameters.Length != numberOfParameters + 1)
         {
             return false;
         }
 
+        return ParametersMatch(potentialEquivalent, method, numberOfParameters) &&
+               HasSupportedTrailingParameter(potentialEquivalent, numberOfParameters);
+    }
+
+    private static bool ParametersMatch(IMethodSymbol potentialEquivalent, IMethodSymbol method, int numberOfParameters)
+    {
         for (var i = 0; i < numberOfParameters; i++)
         {
             if (!SymbolEqualityComparer.Default.Equals(method.Parameters[i].Type, potentialEquivalent.Parameters[i].Type))
@@ -68,12 +82,13 @@ internal static class EquivalentAsynchronousMethodSignatureComparer
                 return false;
         }
 
-        if (potentialEquivalent.Parameters.Length == numberOfParameters + 1 &&
-            !potentialEquivalent.Parameters[numberOfParameters].Type.FullNameIsEqualTo("System.Threading", "CancellationToken"))
-        {
-            return false;
-        }
-
         return true;
+    }
+
+    private static bool HasSupportedTrailingParameter(IMethodSymbol potentialEquivalent, int numberOfParameters)
+    {
+        return potentialEquivalent.Parameters.Length == numberOfParameters ||
+               potentialEquivalent.Parameters[numberOfParameters].Type
+                   .FullNameIsEqualTo("System.Threading", "CancellationToken");
     }
 }
